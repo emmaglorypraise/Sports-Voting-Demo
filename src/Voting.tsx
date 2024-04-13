bcec8898c226d6b73b0a73108b94fd363671c892
emmaglorypraise <emmaglorypraise@gmail.com>
2024-04-13 18:58:26 +0100

    fixed images display

--- src/Voting.tsx
+++ src/Voting.tsx
@@ -84,7 +84,7 @@ const Voting = () => {
               }}
             >
               <img
-                src={`/src/assets/${club.toLowerCase().replace(' ', '')}.png`}
+                src={`/src/assets/${club.replace(' ', '')}.png`}
                 alt={club}
                 className="absolute inset-0 w-full h-full object-cover bg-black"
               />
